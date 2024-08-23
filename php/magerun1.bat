01f9b2bd1cee81a27f4b13aa971e7d05f805b370
Dmitrii Fediuk <dfediuk@gmail.com>
2024-08-23 16:44:13 +0100

    https://github.com/df-windows/tools/issues/9

---
+++ php/magerun1.bat
@@ -0,0 +1,4 @@
+:: 2024-08-23
+:: 1) "Install `n98-magerun`": https://github.com/df-windows/tools/issues/9
+:: 2) "How do I install `n98-magerun` for Magento 1 to Cygwin?": https://mage2.pro/t/6474
+@php "%~dp0magerun1" %*
